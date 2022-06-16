007cfff60e53deb979b59c953abd81129069ba2c
FireTramp <lucaswarwick02@gmail.com>
2022-06-16 16:20:41 +0100

    Further Example phase out / refactor

--- src/main/java/lucaswarwick02/mixin/VanillaOverhaulMixin.java
+++ src/main/java/lucaswarwick02/mixin/VanillaOverhaulMixin.java
@@ -1,6 +1,6 @@
 package lucaswarwick02.mixin;
 
-import lucaswarwick02.ExampleMod;
+import lucaswarwick02.VanillaOverhaulMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class VanillaOverhaulMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		VanillaOverhaulMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
